bfd213eb179b0495a6e8a15552f3096c320aeaa1
Brayan David Lezama Trejo <85713808+itsDavidev@users.noreply.github.com>
2022-05-20 17:01:24 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className='bg-black min-h-screen text-blue-600 p-4'>
+			<h2 className='text-center text-2xl'>
+				Template React + Eslint + Tailwindcss
+			</h2>
+		</div>
+	);
+}
+
+export default App;

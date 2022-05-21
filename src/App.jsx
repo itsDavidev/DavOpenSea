179eadde14cf561f5b671fcf0b266cb42358a0b2
itsDavidev <davidlez2mgk@gmail.com>
2022-05-20 23:04:12 -0500

    ✨ initial app ntfs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,39 @@
+import { useEffect, useState } from 'react';
+import { useMoralisWeb3Api, useMoralis } from 'react-moralis';
+
+async function getDataNfts({ limit, setNFTs, searchNFTs }) {
+	const options = { q: 'Pancake', chain: 'ETH', filter: 'name', limit };
+	const responseNfts = await searchNFTs(options);
+	console.log(responseNfts);
+	await setNFTs(responseNfts.result);
+}
+
+function useNFTs() {
+	const { Moralis } = useMoralis();
+
+	const [limit, setLimit] = useState(10);
+	const [nfts, setNFTs] = useState([]);
+	const Web3Api = useMoralisWeb3Api();
+
+	useEffect(() => {
+		Moralis.start({
+			serverUrl: import.meta.env.VITE_SERVER_URL,
+			appId: import.meta.env.VITE_APP_ID,
+			masterKey: import.meta.env.VITE_MASTER_KEY,
+		});
+	});
+
+	useEffect(() => {
+		getDataNfts({ limit, setNFTs, searchNFTs: Web3Api.token.searchNFTs });
+	}, [limit]);
+
+	return nfts;
+}
+
 function App() {
+	const nfts = useNFTs();
+
+	console.log(nfts);
 	return (
 		<div className='bg-black min-h-screen text-blue-600 p-4'>
 			<h2 className='text-center text-2xl'>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MoralisProvider } from 'react-moralis';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+	<React.StrictMode>
+		<MoralisProvider
+			serverUrl={import.meta.env.VITE_SERVER_URL}
+			appId={import.meta.env.VITE_APP_ID}>
+			<App />
+		</MoralisProvider>
+	</React.StrictMode>
+);
